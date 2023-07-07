5d5cb12297d6eddf4eb584521f451fa5aa7f6949
chunhuizhao478 <chunhui3@illinois.edu>
2023-07-07 11:06:15 -0500

    update-matchpeak-version

--- src/materials/ratestate2d/RateStateFrictionLaw2Dv7.C
+++ src/materials/ratestate2d/RateStateFrictionLaw2Dv7.C
@@ -112,12 +112,6 @@ RateStateFrictionLaw2Dv7::computeInterfaceTractionAndDerivatives()
 
     //*Nodal Mass*
     ///QUAD4 Element
-    //mass_martix assembly of 
-    //_test[_i][_qp] * _density[_qp] * (*_du_dotdot_du)[_qp] * _phi[this->_j][_qp]
-    //where _du_dotdot_du is the spatial derivative of acceleration u_dotdot
-    //this turns out to be M / dt^2 
-    //the following formulation is corrected and used inside moose
-
     Real M = _density[_qp] * len * len * 0.5;
 
     //Initialize Final Traction
@@ -126,195 +120,152 @@ RateStateFrictionLaw2Dv7::computeInterfaceTractionAndDerivatives()
 
     //Initialize Shear Perturbation
     Real Ts_perturb = _Ts_perturb[_qp]; 
-    //Real Ts_perturb = 0.0; 
 
-    //Real x_coord =_q_point[_qp](0);
-    //std::cout<<x_coord<<std::endl;
+    //*Get slip rate and slip from time t-dt/2 and t
+    ///strike direction
+    Real sliprate_strike_tminusdtover2 = _sliprate_strike_old[_qp];
+    Real slip_strike_t = _slip_strike_old[_qp];
 
-    //Specify Condition
-    //_fe_problem.getCurrentExecuteOnFlag()==Moose::LINEAR : Before System Solve
-    //_fe_problem.getCurrentExecuteOnFlag()==Moose::NONE : After System Solve
-    //if (_fe_problem.getCurrentExecuteOnFlag()=="LINEAR"){
+    ///normal direction
+    Real sliprate_normal_tminusdtover2 = _sliprate_normal_old[_qp];
+    Real slip_normal_t = _slip_normal_old[_qp];
+
+    ///mag
+    Real sliprate_mag_tminusdtover2 = _sliprate_mag_old[_qp];
+
+    //Get State Variable at Current Time Step
+    Real statevar_t = _statevar_old[_qp];
+
+    //*Compute Trial Normal Traction and Normal Traction*
+    Real Tn_trial = ( -1.0 * (1.0/_dt) * M * M * ( sliprate_normal_tminusdtover2 + (1.0/_dt) * slip_normal_t) ) / ( len * (M + M) ) + ( M * R_minus_local_normal - M * R_plus_local_normal ) / ( len * (M + M) ) - Tn_o;
+    if (Tn_trial<0)
+    {
+        Tn = Tn_trial;
+    }else{
+        Tn = 0;
+    }
+
+    ///Make Tn positive
+    Tn = abs(Tn);
+
+    //*Compute Trial Shear Traction Along Strike Direction at Current Time Step*
+    Real Ts_trial = ( M * M * sliprate_strike_tminusdtover2 )/( len * _dt * (M + M) ) + (M * R_plus_local_strike - M * R_minus_local_strike) / ( len * ( M + M ) ) + Ts_o + Ts_perturb;
+
+    //*Solve Nonlinear Equation for sliprate at new step t+dt/2*
+    //guess - unknown sliprate of next time step
+    
+    ///Compute known values
+    Real c = len * _dt * ( M + M ) / ( M * M );
+    
+    //Setup outer while loop
+    Real err = 1;
+    Real iter = 1;
+    Real v_h_pre = sliprate_mag_tminusdtover2; //sliprate value at previous time step
+    Real theta_pre = statevar_t; //state variable at previous time step
+    Real dv_pre = abs(sliprate_mag_tminusdtover2); //sliprate predictor initialization 
+    Real dv_corr;  //sliprate corrector initialization 
+    //
+    Real theta_ref=theta_pre; 
+    Real solution; 
+    
+    Real iterr = 1;
+    Real max_iter = 10000;
+    Real er = 1;
+    //Setup inner while loop
+    Real guess_i = sliprate_mag_tminusdtover2;
+    Real residual;
+    Real jacobian;
+    Real guess_j;
+    while ( er > 1e-8 && iterr < max_iter ){  
         
-        /*
-        Before System Solve, compute needed traction
-        */ 
+        //Compute Residual
+        residual = guess_i + c * Tn * mu_friction_law_2Dv7(0.5*(guess_i+sliprate_mag_tminusdtover2), theta_ref, rsf_a, rsf_b, rsf_L, delta_o, f_o) - c * Ts_trial;
 
-        // ///Disp Plus Side
-        // Real alongfaultdisp_strike_plus_t = _alongfaultdisp_strike_plus_old[_qp];
-        // Real alongfaultdisp_normal_plus_t = _alongfaultdisp_normal_plus_old[_qp];
-        // ///Disp Minus Side
-        // Real alongfaultdisp_strike_minus_t = _alongfaultdisp_strike_minus_old[_qp];
-        // Real alongfaultdisp_normal_minus_t = _alongfaultdisp_normal_minus_old[_qp];
+        //Compute Jacobian
+        Real ratioup = rsf_a * exp((f_o+rsf_b*log((delta_o*theta_ref)/(rsf_L)))/(rsf_a));
+        Real ratiodown = 2 * delta_o * sqrt((exp((2*f_o+2*rsf_b*log((delta_o*theta_ref)/(rsf_L)))/(rsf_a))*(0.5*(guess_i+sliprate_strike_tminusdtover2))*(0.5*(guess_i+sliprate_strike_tminusdtover2)))/(4*delta_o*delta_o)+1);
+        jacobian = 1.0 + c * Tn * ratioup / ratiodown;
 
-        // // // // ///Older Value
-        // Real alongfaultdisp_strike_plus_tminust = _alongfaultdisp_strike_plus_older[_qp];
-        // Real alongfaultdisp_normal_plus_tminust = _alongfaultdisp_normal_plus_older[_qp];
-        // Real alongfaultdisp_strike_minus_tminust = _alongfaultdisp_strike_minus_older[_qp];
-        // Real alongfaultdisp_normal_minus_tminust = _alongfaultdisp_normal_minus_older[_qp];
+        //Compute New guess
+        guess_j = guess_i - residual / jacobian;
 
-        //*Get slip rate and slip from time t-dt/2 and t
-        ///strike direction
-        Real sliprate_strike_tminusdtover2 = _sliprate_strike_old[_qp];
-        Real slip_strike_t = _slip_strike_old[_qp];
+        //save
+        solution = guess_j;
 
-        ///normal direction
-        Real sliprate_normal_tminusdtover2 = _sliprate_normal_old[_qp];
-        Real slip_normal_t = _slip_normal_old[_qp];
+        //Compute err
+        er = abs(guess_j - guess_i)/abs(guess_j);
 
-        ///mag
-        Real sliprate_mag_tminusdtover2 = _sliprate_mag_old[_qp];
+        //Update Old guess
+        guess_i = guess_j;
 
-        //Get State Variable at Current Time Step
-        Real statevar_t = _statevar_old[_qp];
-
-        //*Compute Trial Normal Traction and Normal Traction*
-        Real Tn_trial = ( -1.0 * (1.0/_dt) * M * M * ( sliprate_normal_tminusdtover2 + (1.0/_dt) * slip_normal_t) ) / ( len * (M + M) ) + ( M * R_minus_local_normal - M * R_plus_local_normal ) / ( len * (M + M) ) - Tn_o;
-        if (Tn_trial<0)
-        {
-            Tn = Tn_trial;
-        }else{
-            Tn = 0;
+        //printout
+        if (iterr == max_iter){
+            std::cout<<"NOT CONVERGED!"<<std::endl;
         }
 
-        ///Make Tn positive
-        Tn = abs(Tn);
-
-        //*Compute Trial Shear Traction Along Strike Direction at Current Time Step*
-        Real Ts_trial = ( M * M * sliprate_strike_tminusdtover2 )/( len * _dt * (M + M) ) + (M * R_plus_local_strike - M * R_minus_local_strike) / ( len * ( M + M ) ) + Ts_o + Ts_perturb;
-
-        //*Solve Nonlinear Equation for sliprate at new step t+dt/2*
-        //guess - unknown sliprate of next time step
-        
-        ///Compute known values
-        Real c = len * _dt * ( M + M ) / ( M * M );
-        
-        //Setup outer while loop
-        Real err = 1;
-        Real iter = 1;
-        Real v_h_pre = sliprate_mag_tminusdtover2; //sliprate value at previous time step
-        Real theta_pre = statevar_t; //state variable at previous time step
-        Real dv_pre = abs(sliprate_mag_tminusdtover2); //sliprate predictor initialization 
-        Real dv_corr;  //sliprate corrector initialization 
         //
-        Real theta_ref; 
-        Real solution; 
-        while ( err > 1e-5 && iter < 50){
-            
-            //
-            dv_pre = 0.5 * ( abs(sliprate_mag_tminusdtover2) + dv_pre ); //average slip rate predictor
-            //
-            theta_ref = theta_pre * exp(-dv_pre*_dt/rsf_L) + (rsf_L/dv_pre) * (1-exp(-dv_pre*_dt/rsf_L)); //update state variable
-            //
-            Real iterr = 1;
-            Real max_iter = 10000;
-            Real er = 1;
-            //Setup inner while loop
-            Real guess_i = sliprate_mag_tminusdtover2;
-            Real residual;
-            Real jacobian;
-            Real guess_j;
-            while ( er > 1e-8 && iterr < max_iter ){  
-                
-                //Compute Residual
-                residual = guess_i + c * Tn * mu_friction_law_2Dv7(0.5*(guess_i+sliprate_mag_tminusdtover2), theta_ref, rsf_a, rsf_b, rsf_L, delta_o, f_o) - c * Ts_trial;
+        iterr = iterr + 1;
+    
+    }
+    //
+    dv_corr = 0.5 * ( abs(sliprate_mag_tminusdtover2) + abs(guess_j)); //update slip rate corrector
+    // 
+    err = abs(guess_j - v_h_pre)/abs(guess_j);
+    //
+    v_h_pre = guess_j; //pass previous value <- current value
+    //
+    dv_pre = dv_corr; //pass predictor <- corrector
+    //
+    iter = iter + 1;
 
-                //Compute Jacobian
-                Real ratioup = rsf_a * exp((f_o+rsf_b*log((delta_o*theta_ref)/(rsf_L)))/(rsf_a));
-                Real ratiodown = 2 * delta_o * sqrt((exp((2*f_o+2*rsf_b*log((delta_o*theta_ref)/(rsf_L)))/(rsf_a))*(0.5*(guess_i+sliprate_strike_tminusdtover2))*(0.5*(guess_i+sliprate_strike_tminusdtover2)))/(4*delta_o*delta_o)+1);
-                jacobian = 1.0 + c * Tn * ratioup / ratiodown;
+    if (iterr == max_iter){
+        std::cout<<"NOT CONVERGED!"<<std::endl;
+    }
 
-                //Compute New guess
-                guess_j = guess_i - residual / jacobian;
+    
 
-                //save
-                solution = guess_j;
+    Real sliprate_mag_tplusdtover2 = abs(solution); //2D
 
-                //Compute err
-                er = abs(guess_j - guess_i)/abs(guess_j);
+    theta_ref = ( theta_pre + _dt ) / ( 1.0 + _dt * abs(solution) / rsf_L ); //febealg2dot1
 
-                //Update Old guess
-                guess_i = guess_j;
+    Real statevar_tplusdt = theta_ref;
 
-                //printout
-                if (iterr == max_iter){
-                    std::cout<<"NOT CONVERGED!"<<std::endl;
-                }
+    //*Compute shear traction at time t*
+    ///trapezoidal method
+    Real mu_predict = mu_friction_law_2Dv7(0.5*(sliprate_mag_tminusdtover2+sliprate_mag_tplusdtover2), statevar_tplusdt, rsf_a, rsf_b, rsf_L, delta_o, f_o);
+    Real T_mag = Tn * mu_predict;
 
-                //
-                iterr = iterr + 1;
-            
-            }
-            //
-            dv_corr = 0.5 * ( abs(sliprate_mag_tminusdtover2) + abs(guess_j)); //update slip rate corrector
-            // 
-            err = abs(guess_j - v_h_pre)/abs(guess_j);
-            //
-            v_h_pre = guess_j; //pass previous value <- current value
-            //
-            dv_pre = dv_corr; //pass predictor <- corrector
-            //
-            iter = iter + 1;
+    //Get traction component for t
+    Ts = T_mag;
 
-            if (iterr == max_iter){
-                std::cout<<"NOT CONVERGED!"<<std::endl;
-            }
+    //Update Slip Rate and Slip at t + dt/2 or t + dt
+    ///Slip Rate
+    Real sliprate_strike_tplusdtover2 = sliprate_mag_tplusdtover2;
+    Real sliprate_normal_tplusdtover2 = 0.0;
+    Real sliprate_mag_tplusdtover2_updated = sqrt(sliprate_strike_tplusdtover2 * sliprate_strike_tplusdtover2);
 
-        }
+    ///Slip
+    Real slip_strike_tplusdtover2 = slip_strike_t + sliprate_strike_tplusdtover2 * _dt;
+    Real slip_normal_tplusdtover2 = 0.0;
+    Real slip_mag_tplusdtover2    = sqrt(slip_strike_tplusdtover2 * slip_strike_tplusdtover2);
 
-        Real sliprate_mag_tplusdtover2 = solution; //2D
+    //Save Results
+    ///Traction (t)
+    _traction_strike[_qp] = Ts;
+    _traction_normal[_qp] = Tn;
 
-        Real statevar_tplusdt = theta_ref;
+    ///Slip Rate (t+dt/2)
+    _sliprate_strike[_qp] = sliprate_strike_tplusdtover2;
+    _sliprate_normal[_qp] = sliprate_normal_tplusdtover2;
+    _sliprate_mag[_qp]    = sliprate_mag_tplusdtover2_updated;
 
-        //*Compute shear traction at time t*
-        ///trapezoidal method
-        Real mu_predict = mu_friction_law_2Dv7(0.5*(sliprate_mag_tminusdtover2+sliprate_mag_tplusdtover2), statevar_tplusdt, rsf_a, rsf_b, rsf_L, delta_o, f_o);
-        Real T_mag = Tn * mu_predict;
+    ///Slip (t+dt)
+    _slip_strike[_qp] = slip_strike_tplusdtover2;
+    _slip_normal[_qp] = slip_normal_tplusdtover2;
+    _slip_mag[_qp]    = slip_mag_tplusdtover2;
 
-        //Get traction component for t
-        Ts = T_mag;
-
-        //Update Slip Rate and Slip at t + dt/2 or t + dt
-        ///Slip Rate
-        Real sliprate_strike_tplusdtover2 = sliprate_mag_tplusdtover2;
-        Real sliprate_normal_tplusdtover2 = 0.0;
-        Real sliprate_mag_tplusdtover2_updated = sqrt(sliprate_strike_tplusdtover2 * sliprate_strike_tplusdtover2);
-
-        ///Slip
-        Real slip_strike_tplusdtover2 = slip_strike_t + sliprate_strike_tplusdtover2 * _dt;
-        Real slip_normal_tplusdtover2 = 0.0;
-        Real slip_mag_tplusdtover2    = sqrt(slip_strike_tplusdtover2 * slip_strike_tplusdtover2);
-
-        //Save Results
-        ///Traction (t)
-        _traction_strike[_qp] = Ts;
-        _traction_normal[_qp] = Tn;
-
-        ///Slip Rate (t+dt/2)
-        _sliprate_strike[_qp] = sliprate_strike_tplusdtover2;
-        _sliprate_normal[_qp] = sliprate_normal_tplusdtover2;
-        _sliprate_mag[_qp]    = sliprate_mag_tplusdtover2_updated;
-
-        ///Slip (t+dt)
-        _slip_strike[_qp] = slip_strike_tplusdtover2;
-        _slip_normal[_qp] = slip_normal_tplusdtover2;
-        _slip_mag[_qp]    = slip_mag_tplusdtover2;
-
-        ///State Variable (t+dt)
-        _statevar[_qp] = statevar_tplusdt;
-
-    //}
-    //else{
-
-        /*
-        After System Solve, do nothing"
-        */
-
-        //use the latest value
-        // Ts = _traction_strike[_qp];
-        // Tn = _traction_normal[_qp];
-
-    //}
+    ///State Variable (t+dt)
+    _statevar[_qp] = statevar_tplusdt;
 
     //Feed traction into the system
     //Assign back traction in CZM
