7d9c8b997556c08f5898edd176ef1746976b2373
chunhuizhao478 <chunhui3@illinois.edu>
2023-07-13 20:07:56 +0800

    upload-3d-BC

---
+++ examples/ratestate_tpv1013D/BC/main.i
@@ -0,0 +1,346 @@
+[Mesh]
+    [./msh]
+        type = GeneratedMeshGenerator
+        dim = 3
+        nx = 50
+        ny = 50
+        nz = 50
+        xmin = -5000
+        xmax = 5000
+        ymin = -5000
+        ymax = 5000
+        zmin = -5000
+        zmax = 5000
+    [../]
+    [./new_block]
+        type = ParsedSubdomainMeshGenerator
+        input = msh
+        combinatorial_geometry = 'y<0'
+        block_id = 1
+    []
+    #add "Block0_Block1" and "Block1_Block0" interfaces
+    [./split]
+        type = BreakMeshByBlockGenerator
+        input = new_block
+        split_interface = true
+        add_interface_on_two_sides = true
+    []
+[]
+
+[GlobalParams]
+    displacements = 'disp_x disp_y disp_z'
+
+    ##damping ratio 
+    q = 0.1
+
+[]
+
+[Variables]
+    [./disp_x]
+        order = FIRST
+        family = LAGRANGE
+    [../]
+    [./disp_y]
+        order = FIRST
+        family = LAGRANGE
+    [../]
+    [./disp_z]
+        order = FIRST
+        family = LAGRANGE
+    [../]
+[]
+
+[AuxVariables]
+    #restoration force (tag after solve)
+    [./resid_x]
+        order = FIRST
+        family = LAGRANGE
+    [../]
+    [./resid_y]
+        order = FIRST
+        family = LAGRANGE
+    [../] 
+    [./resid_z]
+        order = FIRST
+        family = LAGRANGE
+    [../] 
+    #restoration force damping (tag after solve)
+    [./resid_damp_x]
+        order = FIRST
+        family = LAGRANGE
+    [../]
+    [./resid_damp_y]
+        order = FIRST
+        family = LAGRANGE
+    [../] 
+    [./resid_damp_z]
+        order = FIRST
+        family = LAGRANGE
+    [../] 
+    #interface displacement boundary condition (scale disp_plusminus_(x/y))
+    [disp_plusminus_scaled_x]
+        order = FIRST
+        family = LAGRANGE
+    []
+    [disp_plusminus_scaled_y]
+        order = FIRST
+        family = LAGRANGE
+    []
+    [disp_plusminus_scaled_z]
+        order = FIRST
+        family = LAGRANGE
+    []
+    #traction
+    [traction_strike]
+        order = CONSTANT
+        family = MONOMIAL
+    []
+    #sliprate
+    [sliprate_mag]
+        order = CONSTANT
+        family = MONOMIAL
+    []
+    #slip
+    [slip_strike]
+        order = CONSTANT
+        family = MONOMIAL
+    []
+    #statevar
+    [statevar]
+        order = CONSTANT
+        family = MONOMIAL
+    []
+[]
+
+[AuxKernels]
+    #obtain system residuals by tagging
+    [restore_x]
+        type = TagVectorAux
+        vector_tag = 'restore_tag'
+        v = 'disp_x'
+        variable = 'resid_x'
+        execute_on = 'TIMESTEP_END'
+    []
+    [restore_y]
+        type = TagVectorAux
+        vector_tag = 'restore_tag'
+        v = 'disp_y'
+        variable = 'resid_y'
+        execute_on = 'TIMESTEP_END'
+    []
+    [restore_z]
+        type = TagVectorAux
+        vector_tag = 'restore_tag'
+        v = 'disp_z'
+        variable = 'resid_z'
+        execute_on = 'TIMESTEP_END'
+    []
+    #damping
+    [restore_damp_x]
+        type = TagVectorAux
+        vector_tag = 'restore_dampx_tag'
+        v = 'disp_x'
+        variable = 'resid_damp_x'
+        execute_on = 'TIMESTEP_END'
+    []
+    [restore_damp_y]
+        type = TagVectorAux
+        vector_tag = 'restore_dampy_tag'
+        v = 'disp_y'
+        variable = 'resid_damp_y'
+        execute_on = 'TIMESTEP_END'
+    []
+    [restore_damp_z]
+        type = TagVectorAux
+        vector_tag = 'restore_dampz_tag'
+        v = 'disp_z'
+        variable = 'resid_damp_z'
+        execute_on = 'TIMESTEP_END'
+    []
+[]
+
+[Problem]
+    extra_tag_vectors = 'restore_tag restore_dampx_tag restore_dampy_tag restore_dampz_tag'
+[]
+
+[Modules]
+    [./TensorMechanics]
+        [./Master]
+        [./all]
+            strain = SMALL
+            displacements = 'disp_x disp_y disp_z'
+            extra_vector_tags = 'restore_tag'
+        [../]
+        [../]
+    [../]
+[]
+
+[Kernels]
+    [./inertia_x]
+        type = InertialForce
+        use_displaced_mesh = false
+        variable = disp_x
+    []
+    [./inertia_y]
+        type = InertialForce
+        use_displaced_mesh = false
+        variable = disp_y
+    []
+    [./inertia_z]
+        type = InertialForce
+        use_displaced_mesh = false
+        variable = disp_z
+    []
+    [./Reactionx]
+        type = StiffPropDamping
+        variable = disp_x
+        component = 0
+        extra_vector_tags = 'restore_dampx_tag'
+    []
+    [./Reactiony]
+        type = StiffPropDamping
+        variable = disp_y
+        component = 1
+        extra_vector_tags = 'restore_dampy_tag'
+    []
+    [./Reactionz]
+        type = StiffPropDamping
+        variable = disp_z
+        component = 1
+        extra_vector_tags = 'restore_dampz_tag'
+    []
+[]
+
+[Materials]
+    [elasticity]
+        type = ComputeIsotropicElasticityTensor
+        lambda = 32.04e9
+        shear_modulus = 32.04e9
+        use_displaced_mesh = false
+    []
+    [stress]
+        type = ComputeLinearElasticStress
+    []
+    [density]
+        type = GenericConstantMaterial
+        prop_names = density
+        prop_values = 2670
+    []
+[]
+
+[UserObjects]
+    #evalute system residual after system solve before auxkernels
+    [recompute_residual_tag]
+        type = ResidualEvaluationUserObject
+        vector_tag = 'restore_tag'
+        force_preaux = true
+        execute_on = 'TIMESTEP_END'
+    []
+    [recompute_dampx_residual_tag]
+        type = ResidualEvaluationUserObject
+        vector_tag = 'restore_dampx_tag'
+        force_preaux = true
+        execute_on = 'TIMESTEP_END'
+    []
+    [recompute_dampy_residual_tag]
+        type = ResidualEvaluationUserObject
+        vector_tag = 'restore_dampy_tag'
+        force_preaux = true
+        execute_on = 'TIMESTEP_END'
+    []
+    [recompute_dampz_residual_tag]
+        type = ResidualEvaluationUserObject
+        vector_tag = 'restore_dampz_tag'
+        force_preaux = true
+        execute_on = 'TIMESTEP_END'
+    []
+[]
+
+[BCs]
+    #assign displacement boundary condition
+    [./matchval_primary_x]
+        type = MatchedValueBC
+        variable = disp_x
+        v = disp_plusminus_scaled_x
+        boundary = 'Block0_Block1'
+    []
+    [./matchval_secondary_x]
+        type = MatchedValueBC
+        variable = disp_x
+        v = disp_plusminus_scaled_x
+        boundary = 'Block1_Block0'
+    []
+    [./matchval_primary_y]
+        type = MatchedValueBC
+        variable = disp_y
+        v = disp_plusminus_scaled_y
+        boundary = 'Block0_Block1'
+    []
+    [./matchval_secondary_y]
+        type = MatchedValueBC
+        variable = disp_y
+        v = disp_plusminus_scaled_y
+        boundary = 'Block1_Block0'
+    []
+    [./matchval_primary_z]
+        type = MatchedValueBC
+        variable = disp_z
+        v = disp_plusminus_scaled_z
+        boundary = 'Block0_Block1'
+    []
+    [./matchval_secondary_z]
+        type = MatchedValueBC
+        variable = disp_z
+        v = disp_plusminus_scaled_z
+        boundary = 'Block1_Block0'
+    []
+[]
+
+[Executioner]
+    type = Transient
+    dt = 0.005
+    end_time = 1.5
+    # num_steps = 10
+    [TimeIntegrator]
+        type = CentralDifference
+        solve_type = lumped
+    []
+[]
+
+[Outputs]
+    exodus = true
+    interval = 10
+[]
+
+[MultiApps]
+    #allocate transfer from mainApp to subApp
+    [./sub_app]
+      type = TransientMultiApp
+      positions = '0 0 0'
+      input_files = 'sub.i'
+      execute_on = 'INITIAL TIMESTEP_BEGIN'
+    [../]
+  []
+
+[Transfers]
+    #get displacement residuals from subApp to mainApp
+    [pull_resid]
+        type = MultiAppCopyTransfer
+        from_multi_app = sub_app
+        source_variable = 'disp_plusminus_sub_scaled_x disp_plusminus_sub_scaled_y disp_plusminus_sub_scaled_z traction_sub_strike sliprate_sub_mag slip_sub_strike statevar_sub'
+        variable = 'disp_plusminus_scaled_x disp_plusminus_scaled_y disp_plusminus_scaled_z traction_strike sliprate_mag slip_strike statevar'
+        execute_on = 'INITIAL TIMESTEP_BEGIN'
+    []
+    #push system residual vector from mainApp to subApp
+    [push_disp]
+        type = MultiAppCopyTransfer
+        to_multi_app = sub_app
+        source_variable = 'resid_x resid_y resid_z resid_damp_x resid_damp_y resid_damp_z'
+        variable = 'resid_sub_x resid_sub_y resid_sub_z resid_damp_sub_x resid_damp_sub_y resid_damp_sub_z'
+        execute_on = 'INITIAL TIMESTEP_BEGIN'
+    []
+[]
+
+# [Debug]
+#     show_execution_order = ALWAYS
+# []
---
+++ src/materials/ratestate3d/RateStateFrictionLaw3DAsBC.C
@@ -0,0 +1,386 @@
+//Implementation of Rate-and-State Friction Law in 3D
+
+#include "RateStateFrictionLaw3DAsBC.h"
+#include "InterfaceKernel.h"
+#include "NestedSolve.h"
+#include "libmesh/utility.h"
+#include "RankTwoTensor.h"
+#include "libmesh/vector_value.h"
+#include "FEProblem.h"
+
+registerMooseObject("farmsApp", RateStateFrictionLaw3DAsBC);
+
+InputParameters
+RateStateFrictionLaw3DAsBC::validParams()
+{ 
+  //Tn_o,Ts_o,Vini,statevarini are defined in "CZMComputeLocalTractionTotalBaseRSF3D"
+  InputParameters params = CZMComputeLocalTractionTotalBaseRSF3D::validParams();
+  params.addClassDescription("Rate-and-State Frictional Law.");
+  params.addParam<Real>("len",200,"element length");
+  params.addParam<Real>("f_o",0.6,"rate-and-state friction coefficients");
+  params.addParam<Real>("rsf_a",0.008,"rate-and-state friction coefficients");
+  params.addParam<Real>("rsf_b",0.012,"rate-and-state friction coefficients");
+  params.addParam<Real>("rsf_L",0.02,"rate-and-state friction coefficients");
+  params.addParam<Real>("delta_o",1e-6,"slip rate parameter");
+  params.addRequiredCoupledVar("reaction_rsf_x","reaction in x dir");
+  params.addRequiredCoupledVar("reaction_rsf_y","reaction in y dir");
+  params.addRequiredCoupledVar("reaction_rsf_z","reaction in z dir");
+  params.addRequiredCoupledVar("Ts_perturb","shear stress perturbation in strike dir");
+  return params;
+}
+
+RateStateFrictionLaw3DAsBC::RateStateFrictionLaw3DAsBC(const InputParameters & parameters)
+  : CZMComputeLocalTractionTotalBaseRSF3D(parameters),
+    _len(getParam<Real>("len")),
+    _f_o(getParam<Real>("f_o")),
+    _rsf_a(getParam<Real>("rsf_a")),
+    _rsf_b(getParam<Real>("rsf_b")),
+    _rsf_L(getParam<Real>("rsf_L")),
+    _delta_o(getParam<Real>("delta_o")),
+    _density(getMaterialPropertyByName<Real>(_base_name + "density")),
+    _rot(getMaterialPropertyByName<RankTwoTensor>(_base_name + "czm_total_rotation")),
+    _reaction_rsf_x(coupledValue("reaction_rsf_x")),
+    _reaction_rsf_y(coupledValue("reaction_rsf_y")),
+    _reaction_rsf_z(coupledValue("reaction_rsf_z")),
+    _reaction_rsf_neighbor_x(coupledNeighborValue("reaction_rsf_x")),
+    _reaction_rsf_neighbor_y(coupledNeighborValue("reaction_rsf_y")),
+    _reaction_rsf_neighbor_z(coupledNeighborValue("reaction_rsf_z")),
+    _Ts_perturb(coupledValue("Ts_perturb")),
+    _Ts_perturb_old(coupledValueOld("Ts_perturb")),
+    _alongfaultvel_strike_plus_old(getMaterialPropertyOldByName<Real>("alongfaultvel_strike_plus")),
+    _alongfaultvel_strike_minus_old(getMaterialPropertyOldByName<Real>("alongfaultvel_strike_minus")),
+    _alongfaultvel_normal_plus_old(getMaterialPropertyOldByName<Real>("alongfaultvel_normal_plus")),
+    _alongfaultvel_normal_minus_old(getMaterialPropertyOldByName<Real>("alongfaultvel_normal_minus")),
+    _alongfaultvel_dip_plus_old(getMaterialPropertyOldByName<Real>("alongfaultvel_dip_plus")),
+    _alongfaultvel_dip_minus_old(getMaterialPropertyOldByName<Real>("alongfaultvel_dip_minus")),
+    _alongfaultdisp_strike_plus_old(getMaterialPropertyOldByName<Real>("alongfaultdisp_strike_plus")),
+    _alongfaultdisp_strike_minus_old(getMaterialPropertyOldByName<Real>("alongfaultdisp_strike_minus")),
+    _alongfaultdisp_normal_plus_old(getMaterialPropertyOldByName<Real>("alongfaultdisp_normal_plus")),
+    _alongfaultdisp_normal_minus_old(getMaterialPropertyOldByName<Real>("alongfaultdisp_normal_minus")),
+    _alongfaultdisp_dip_plus_old(getMaterialPropertyOldByName<Real>("alongfaultdisp_dip_plus")),
+    _alongfaultdisp_dip_minus_old(getMaterialPropertyOldByName<Real>("alongfaultdisp_dip_minus")),
+    _alongfaultdisp_strike_plus_older(getMaterialPropertyOlderByName<Real>("alongfaultdisp_strike_plus")),
+    _alongfaultdisp_normal_plus_older(getMaterialPropertyOlderByName<Real>("alongfaultdisp_normal_plus")),
+    _alongfaultdisp_dip_plus_older(getMaterialPropertyOlderByName<Real>("alongfaultdisp_dip_plus")),
+    _alongfaultdisp_strike_minus_older(getMaterialPropertyOlderByName<Real>("alongfaultdisp_strike_minus")),
+    _alongfaultdisp_normal_minus_older(getMaterialPropertyOlderByName<Real>("alongfaultdisp_normal_minus")),
+    _alongfaultdisp_dip_minus_older(getMaterialPropertyOlderByName<Real>("alongfaultdisp_dip_minus")),
+    _sliprate_strike_old(getMaterialPropertyOldByName<Real>("sliprate_strike")),
+    _sliprate_normal_old(getMaterialPropertyOldByName<Real>("sliprate_normal")),
+    _sliprate_dip_old(getMaterialPropertyOldByName<Real>("sliprate_dip")),
+    _sliprate_mag_old(getMaterialPropertyOldByName<Real>("sliprate_mag")),
+    _sliprate_predict(getMaterialPropertyOldByName<Real>("sliprate_predict")),
+    _slip_strike_old(getMaterialPropertyOldByName<Real>("slip_strike")),
+    _slip_normal_old(getMaterialPropertyOldByName<Real>("slip_normal")),
+    _slip_dip_old(getMaterialPropertyOldByName<Real>("slip_dip")),
+    _statevar_old(getMaterialPropertyOldByName<Real>("statevar")),
+    _statevar_older(getMaterialPropertyOlderByName<Real>("statevar")),
+    _traction_strike_old(getMaterialPropertyOldByName<Real>("traction_strike")),
+    _traction_normal_old(getMaterialPropertyOldByName<Real>("traction_normal")),
+    _traction_dip_old(getMaterialPropertyOldByName<Real>("traction_dip"))
+{
+}
+
+//Define Frictional Law (function of sliprate and statevar)
+double mu_friction_law_3DAsBC(Real sliprate, Real statevar, Real rsf_a, Real rsf_b, Real rsf_L, Real delta_o, Real f_o)
+{
+  double mu = 0;
+  mu = rsf_a * asinh( sliprate/(2*delta_o) * exp((f_o + rsf_b * log(delta_o * statevar/rsf_L))/rsf_a) );
+  return mu;
+}
+
+void
+RateStateFrictionLaw3DAsBC::computeInterfaceTractionAndDerivatives()
+{   
+    //Define Parameters
+    Real len = _len;
+    Real f_o = _f_o;
+    Real rsf_a = _rsf_a;
+    Real rsf_b = _rsf_b;
+    Real rsf_L = _rsf_L;
+    Real delta_o = _delta_o;
+    Real Tn_o = _Tn_o;
+    Real Ts_o = _Ts_o;
+    Real Td_o = _Td_o;
+    
+    //*Restoration Force*
+    ///Define in global coordinate
+    //current time step 
+    RealVectorValue R_plus_global(-_reaction_rsf_x[_qp],-_reaction_rsf_y[_qp], -_reaction_rsf_z[_qp]);
+    RealVectorValue R_minus_global(-_reaction_rsf_neighbor_x[_qp],-_reaction_rsf_neighbor_y[_qp], -_reaction_rsf_neighbor_z[_qp]);
+
+    ///Rotate in local coordinate
+    //current time step
+    RealVectorValue R_plus_local = _rot[_qp].transpose() * R_plus_global;
+    RealVectorValue R_minus_local = _rot[_qp].transpose() * R_minus_global;
+
+    ///Get Components
+    //current time step
+    Real R_plus_local_strike  = R_plus_local(1);
+    Real R_plus_local_normal  = R_plus_local(0);
+    Real R_plus_local_dip     = R_plus_local(2);
+
+    Real R_minus_local_strike = R_minus_local(1);
+    Real R_minus_local_normal = R_minus_local(0);
+    Real R_minus_local_dip    = R_minus_local(2);
+
+    //*Nodal Mass*
+    ///HEX8 Element
+    Real M = _density[_qp] * len * len * len * 0.5;
+
+    //Initialize Final Traction
+    Real Ts = 0.0; //strike
+    Real Tn = 0.0; //normal
+    Real Td = 0.0; //dip
+
+    //Initialize shear stress perturbation
+    Real Ts_perturb = 0.0;
+
+    Real len_len = ( 1.0 * len ) * ( 1.0 * len );
+
+    //Specify Condition
+    //_fe_problem.getCurrentExecuteOnFlag()==Moose::NONE : After System Solve
+    //_fe_problem.getCurrentExecuteOnFlag()==Moose::LINEAR : Before System Solve
+    if (_fe_problem.getCurrentExecuteOnFlag()=="LINEAR"){
+        
+        /*
+        Before System Solve, quantities are allowed to be updated"
+        */
+        
+        //Assign shear perturbation at t, t-dt
+        Ts_perturb = _Ts_perturb[_qp];
+
+        ///Disp Plus Side
+        Real alongfaultdisp_strike_plus_t = _alongfaultdisp_strike_plus_old[_qp];
+        Real alongfaultdisp_normal_plus_t = _alongfaultdisp_normal_plus_old[_qp];
+        Real alongfaultdisp_dip_plus_t    = _alongfaultdisp_dip_plus_old[_qp];
+        ///Disp Minus Side
+        Real alongfaultdisp_strike_minus_t = _alongfaultdisp_strike_minus_old[_qp];
+        Real alongfaultdisp_normal_minus_t = _alongfaultdisp_normal_minus_old[_qp];
+        Real alongfaultdisp_dip_minus_t = _alongfaultdisp_dip_minus_old[_qp];
+
+        //Older Value
+        //Plus Side
+        Real alongfaultdisp_strike_plus_tminust = _alongfaultdisp_strike_plus_older[_qp];
+        Real alongfaultdisp_normal_plus_tminust = _alongfaultdisp_normal_plus_older[_qp];
+        Real alongfaultdisp_dip_plus_tminust = _alongfaultdisp_dip_plus_older[_qp];
+        //Minus Side
+        Real alongfaultdisp_strike_minus_tminust = _alongfaultdisp_strike_minus_older[_qp];
+        Real alongfaultdisp_normal_minus_tminust = _alongfaultdisp_normal_minus_older[_qp];
+        Real alongfaultdisp_dip_minus_tminust = _alongfaultdisp_dip_minus_older[_qp];
+    
+        //*Get slip rate and slip from time t-dt/2 and t
+        ///strike direction
+        Real sliprate_strike_tminusdtover2 = _sliprate_strike_old[_qp];
+        Real slip_strike_t = _slip_strike_old[_qp];
+
+        ///dip direction
+        Real sliprate_dip_tminusdtover2 = _sliprate_dip_old[_qp];
+        Real slip_dip_t = _slip_dip_old[_qp];
+
+        ///normal direction
+        Real sliprate_normal_tminusdtover2 = _sliprate_normal_old[_qp];
+        Real slip_normal_t = _slip_normal_old[_qp];
+
+        ///mag
+        Real sliprate_mag_tminusdtover2 = _sliprate_mag_old[_qp];
+
+        //Get State Variable at Current Time Step
+        Real statevar_t = _statevar_old[_qp];
+
+        //*Compute Trial Normal Traction and Normal Traction*
+        Real Tn_trial = ( -1.0 * (1.0/_dt) * M * M * ( sliprate_normal_tminusdtover2 + (1.0/_dt) * slip_normal_t) ) / ( len_len * (M + M) ) + ( M * R_minus_local_normal - M * R_plus_local_normal ) / ( len_len * (M + M) ) - Tn_o;
+        if (Tn_trial<0)
+        {
+            Tn = Tn_trial;
+        }else{
+            Tn = 0;
+        }
+
+        ///Make Tn positive
+        Tn = abs(Tn);
+
+        //*Compute Trial Shear Traction Along Strike Direction at Current Time Step*
+        Real Ts_trial = ( M * M * sliprate_strike_tminusdtover2 )/( len_len * _dt * (M + M) ) + (M * R_plus_local_strike - M * R_minus_local_strike) / ( len_len * ( M + M ) ) + Ts_o + Ts_perturb;
+        Real Td_trial = ( M * M * sliprate_dip_tminusdtover2    )/( len_len * _dt * (M + M) ) + (M * R_plus_local_dip    - M * R_minus_local_dip   ) / ( len_len * ( M + M ) ) + Td_o;
+        Real Tmag_trial = sqrt(Ts_trial*Ts_trial+Td_trial*Td_trial);
+
+        //Setup outer while loop
+        Real err = 1;
+        Real iter = 1;
+        Real v_h_pre = sliprate_mag_tminusdtover2; //sliprate value at previous time step
+        Real theta_pre = statevar_t; //state variable at previous time step
+        Real dv_pre = abs(sliprate_mag_tminusdtover2); //sliprate predictor initialization 
+        Real dv_corr;  //sliprate corrector initialization 
+        //
+        Real theta_ref; 
+        Real solution; 
+        //
+        Real c = len_len * _dt * ( M + M ) / (M * M);
+        while ( err > 1e-5 && iter < 50){
+            
+            //
+            dv_pre = 0.5 * ( abs(sliprate_mag_tminusdtover2) + dv_pre ); //average slip rate predictor
+            //
+            //theta_ref = ( theta_pre + _dt ) / ( 1.0 + _dt * dv_pre / rsf_L ); //febealg2dot1
+            theta_ref = theta_pre * exp(-dv_pre*_dt/rsf_L) + (rsf_L/dv_pre) * (1-exp(-dv_pre*_dt/rsf_L)); //update state variable
+            //
+            Real iterr = 1;
+            Real max_iter = 10000;
+            Real er = 1;
+            //Setup inner while loop
+            Real guess_i = sliprate_mag_tminusdtover2;
+            Real residual;
+            Real jacobian;
+            Real guess_j;
+            while ( er > 1e-8 && iterr < max_iter ){  
+                
+                //Compute Residual
+                residual = guess_i + c * Tn * mu_friction_law_3DAsBC(0.5*(guess_i+sliprate_mag_tminusdtover2), theta_ref, rsf_a, rsf_b, rsf_L, delta_o, f_o) - c * Tmag_trial;
+
+                //Compute Jacobian
+                Real ratioup = rsf_a * exp((f_o+rsf_b*log((delta_o*theta_ref)/(rsf_L)))/(rsf_a));
+                Real ratiodown = 2 * delta_o * sqrt((exp((2*f_o+2*rsf_b*log((delta_o*theta_ref)/(rsf_L)))/(rsf_a))*(0.5*(guess_i+sliprate_strike_tminusdtover2))*(0.5*(guess_i+sliprate_strike_tminusdtover2)))/(4*delta_o*delta_o)+1);
+                jacobian = 1.0 + c * Tn * ratioup / ratiodown;
+
+                //Compute New guess
+                guess_j = guess_i - residual / jacobian;
+
+                //save
+                solution = guess_j;
+
+                //Compute err
+                er = abs(guess_j - guess_i)/abs(guess_j);
+
+                //Update Old guess
+                guess_i = guess_j;
+
+                //printout
+                // if (x_coord > 0 && x_coord < 30){
+                //     std::cout<<"inner er: "<< er <<std::endl;
+                // }
+                if (iterr == max_iter){
+                    std::cout<<"NOT CONVERGED!"<<std::endl;
+                }
+
+                //
+                iterr = iterr + 1;
+            
+            }
+            //
+            dv_corr = 0.5 * ( abs(sliprate_mag_tminusdtover2) + abs(guess_j)); //update slip rate corrector
+            // 
+            err = abs(guess_j - v_h_pre)/abs(guess_j);
+            //
+            v_h_pre = guess_j; //pass previous value <- current value
+            //
+            dv_pre = dv_corr; //pass predictor <- corrector
+            //
+            iter = iter + 1;
+
+            // if (x_coord > 0 && x_coord < 30){
+            //     std::cout<<"outer err: "<< er <<std::endl;
+            // }
+
+        }
+
+        Real sliprate_mag_tplusdtover2 = solution; 
+        
+        // theta_ref = ( theta_pre + _dt ) / ( 1.0 + _dt * dv_pre / rsf_L ); //febealg2dot1
+        //theta_ref = ( _statevar_older[_qp] + 2 * _dt ) / ( 1.0 + 2 * _dt * dv_pre / rsf_L );
+        Real statevar_tplusdt = theta_ref;
+
+        //*Compute shear traction at time t*
+        ///trapezoidal method //?
+        Real mu_predict = mu_friction_law_3DAsBC(0.5*(sliprate_mag_tminusdtover2+sliprate_mag_tplusdtover2), theta_ref, rsf_a, rsf_b, rsf_L, delta_o, f_o);
+        Real T_mag = Tn * mu_predict;
+
+        ///Get Components
+        Ts = T_mag * ( Ts_trial / Tmag_trial );
+        Td = T_mag * ( Td_trial / Tmag_trial );
+
+        //*Compute quantities at time t + dt/2 or t + dt
+        ////DISP
+        Real du_strike_plus_t  =  alongfaultdisp_strike_plus_t  - alongfaultdisp_strike_plus_tminust  + _dt * _dt / M * (R_plus_local_strike  - len_len * (Ts - Ts_o - Ts_perturb));
+        Real du_normal_plus_t  =  alongfaultdisp_normal_plus_t  - alongfaultdisp_normal_plus_tminust  + _dt * _dt / M * (R_plus_local_normal  - len_len * (Tn - Tn_o));
+        Real du_dip_plus_t     =  alongfaultdisp_dip_plus_t     - alongfaultdisp_dip_plus_tminust     + _dt * _dt / M * (R_plus_local_dip     - len_len * (Td - Td_o));
+        
+        Real du_strike_minus_t =  alongfaultdisp_strike_minus_t  - alongfaultdisp_strike_minus_tminust + _dt * _dt / M * (R_minus_local_strike + len_len * (Ts - Ts_o - Ts_perturb));
+        Real du_normal_minus_t =  alongfaultdisp_normal_minus_t  - alongfaultdisp_normal_minus_tminust + _dt * _dt / M * (R_minus_local_normal + len_len * (Tn - Tn_o));
+        Real du_dip_minus_t    =  alongfaultdisp_dip_minus_t     - alongfaultdisp_dip_minus_tminust    + _dt * _dt / M * (R_minus_local_dip    + len_len * (Td - Td_o));
+
+        Real alongfaultdisp_strike_plus_tplusdt  = alongfaultdisp_strike_plus_t  + du_strike_plus_t;
+        Real alongfaultdisp_normal_plus_tplusdt  = alongfaultdisp_normal_plus_t  + du_normal_plus_t;
+        Real alongfaultdisp_dip_plus_tplusdt     = alongfaultdisp_dip_plus_t     + du_dip_plus_t;
+        
+        Real alongfaultdisp_strike_minus_tplusdt = alongfaultdisp_strike_minus_t + du_strike_minus_t;
+        Real alongfaultdisp_normal_minus_tplusdt = alongfaultdisp_normal_minus_t + du_normal_minus_t;
+        Real alongfaultdisp_dip_minus_tplusdt    = alongfaultdisp_dip_minus_t    + du_dip_minus_t;
+
+        //Update Slip Rate and Slip at t + dt/2 or t + dt
+        ///Slip Rate
+        Real sliprate_strike_tplusdtover2 = sliprate_mag_tplusdtover2 * ( Ts_trial / Tmag_trial );
+        Real sliprate_normal_tplusdtover2 = 0.0;
+        Real sliprate_dip_tplusdtover2    = sliprate_mag_tplusdtover2 * ( Td_trial / Tmag_trial );
+
+        ///Slip
+        Real slip_strike_tplusdtover2 = slip_strike_t + sliprate_strike_tplusdtover2 * _dt;
+        Real slip_normal_tplusdtover2 = 0.0;
+        Real slip_dip_tplusdtover2    = slip_dip_t + sliprate_dip_tplusdtover2 * _dt;
+
+        //Save Results
+        ///Traction (t)
+        _traction_strike[_qp] = Ts;
+        _traction_normal[_qp] = Tn;
+        _traction_dip[_qp] = Td;
+
+        ///Disp (t+dt)
+        _alongfaultdisp_strike_plus[_qp] = alongfaultdisp_strike_plus_tplusdt;
+        _alongfaultdisp_normal_plus[_qp] = alongfaultdisp_normal_plus_tplusdt;
+        _alongfaultdisp_dip_plus[_qp]    = alongfaultdisp_dip_plus_tplusdt;
+
+        _alongfaultdisp_strike_minus[_qp] = alongfaultdisp_strike_minus_tplusdt;
+        _alongfaultdisp_normal_minus[_qp] = alongfaultdisp_normal_minus_tplusdt;
+        _alongfaultdisp_dip_minus[_qp]    = alongfaultdisp_dip_minus_tplusdt;
+
+        ///Slip Rate (t+dt/2)
+        _sliprate_strike[_qp] = sliprate_strike_tplusdtover2;
+        _sliprate_normal[_qp] = sliprate_normal_tplusdtover2;
+        _sliprate_dip[_qp]    = sliprate_dip_tplusdtover2;
+        _sliprate_mag[_qp]    = sliprate_mag_tplusdtover2;
+
+        ///Slip (t+dt)
+        _slip_strike[_qp] = slip_strike_tplusdtover2;
+        _slip_normal[_qp] = slip_normal_tplusdtover2;
+        _slip_dip[_qp]    = slip_dip_tplusdtover2;
+        _slip_mag[_qp]    = sqrt(slip_strike_tplusdtover2*slip_strike_tplusdtover2+slip_dip_tplusdtover2*slip_dip_tplusdtover2);
+
+        ///State Variable (t+dt)
+        _statevar[_qp] = statevar_tplusdt;
+
+    }
+    else{
+
+        /*
+        After System Solve, keep the traction same as last step
+        */
+
+        //get latest value
+        Ts = _traction_strike[_qp];
+        Tn = _traction_normal[_qp];
+        Td = _traction_dip[_qp];
+
+    }
+
+    //Feed traction into the system
+    //Assign back traction in CZM
+    RealVectorValue traction;
+
+    traction(0) = 0.0; 
+    traction(1) = 0.0; 
+    traction(2) = 0.0;
+
+    _interface_traction[_qp] = traction;
+    _dinterface_traction_djump[_qp] = 0;   
+
+}
