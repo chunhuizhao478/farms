5f15cbe5355e5c0402a9c5ae3be0ca30781d1041
chunhuizhao478 <chunhui3@illinois.edu>
2023-09-23 21:49:47 -0500

    sync post processing

--- postprocess/2d/sliptimehist.m
+++ postprocess/2d/sliptimehist.m
@@ -2,22 +2,24 @@ clear all; close all; clc;
 
 %uguca results
 % data0uguca = load("./TPV101_Nx/TPV101_Nx_50and25m/TPV101_Nx2880_s2.00_tf0.35_npc1-DataFiles/vel0.txt");
-datauguca = load("./TPV101_Nx/TPV101_Nx_50and25m/TPV101_Nx1440_s2.00_tf0.35_npc1-DataFiles/disp0.txt");
-% datauguca = load("./TPV101_Nx/TPV101_Nx720_s2.00_tf0.35_npc1/TPV101_Nx720_s2.00_tf0.35_npc1-DataFiles/disp0.txt");
-% ptrs = [311 360 411]; %100m
-ptrs = [620 721 822 670 772]; %50m
+% datauguca = load("./TPV101_Nx/TPV101_Nx_50and25m/TPV101_Nx1440_s2.00_tf0.35_npc1-DataFiles/disp0.txt");
+datauguca = load("./TPV101_Nx/TPV101_Nx720_s2.00_tf0.35_npc1/TPV101_Nx720_s2.00_tf0.35_npc1-DataFiles/disp0.txt");
+ptrs = [310 361 412 335 387 285 437]; %100m
+% ptrs = [622 721 820 671 771 570 872]; %50m
 time_max = 4;
 num_max = 40;
-meshsize = "50m";
-lenged1 = "uguca-2d-50m";
-legend2 = "moose-2d-50m";
-filename = "50m_921_damp05";
+meshsize = "100m";
+lenged1 = "uguca-2d-100m";
+legend2 = "moose-2d-100m";
+filename = "100m_922_uguca_setup";
 datauguca_time = linspace(0,time_max,num_max);
 datauguca_ptr1 = datauguca(:,ptrs(1));
 datauguca_ptr2 = datauguca(:,ptrs(2));
 datauguca_ptr3 = datauguca(:,ptrs(3));
 datauguca_ptr4 = datauguca(:,ptrs(4));
 datauguca_ptr5 = datauguca(:,ptrs(5));
+datauguca_ptr6 = datauguca(:,ptrs(6));
+datauguca_ptr7 = datauguca(:,ptrs(7));
 
 %moose results
 datatime = load("./files/"+filename+"/time.txt");
@@ -26,6 +28,8 @@ datasliprate_ptr2 = load("./files/"+filename+"/slip_0strike0dot75dip.txt");
 datasliprate_ptr3 = load("./files/"+filename+"/slip_pos5kmstrike0dot75dip.txt");
 datasliprate_ptr4 = load("./files/"+filename+"/slip_neg2dot5kmstrike0dot75dip.txt");
 datasliprate_ptr5 = load("./files/"+filename+"/slip_pos2dot5kmstrike0dot75dip.txt");
+datasliprate_ptr6 = load("./files/"+filename+"/slip_neg7dot5kmstrike0dot75dip.txt");
+datasliprate_ptr7 = load("./files/"+filename+"/slip_pos7dot5kmstrike0dot75dip.txt");
 
 %figures
 %%sliprate
@@ -93,4 +97,31 @@ legend(lenged1,legend2,"FontSize",15,'Location','northwest');
 ax = gca;
 ax.FontSize = 15; 
 saveas(gcf,'./plots/slip_ptr5_'+meshsize+'.png')
+
+figure(6);
+plot(datauguca_time(1:num_max),datauguca_ptr6(1:num_max)*2,'r-'); hold on;
+plot(datatime(1:4:end),datasliprate_ptr6(1:4:end),'b-')
+xlim([0,time_max])
+ylim([0,7.0])
+title("Slip Time History at -2.5km","FontSize",30)
+xlabel("time (s)","FontSize",25)
+ylabel("slip (m)","FontSize",25)
+legend(lenged1,legend2,"FontSize",15,'Location','northwest');
+ax = gca;
+ax.FontSize = 15; 
+saveas(gcf,'./plots/slip_ptr6_'+meshsize+'.png')
+
+figure(7);
+plot(datauguca_time(1:num_max),datauguca_ptr7(1:num_max)*2,'r-'); hold on;
+plot(datatime(1:4:end),datasliprate_ptr7(1:4:end),'b-')
+xlim([0,time_max])
+ylim([0,7.0])
+title("Slip Time History at 2.5km","FontSize",30)
+xlabel("time (s)","FontSize",25)
+ylabel("slip (m)","FontSize",25)
+legend(lenged1,legend2,"FontSize",15,'Location','northwest');
+ax = gca;
+ax.FontSize = 15; 
+saveas(gcf,'./plots/slip_ptr7_'+meshsize+'.png')
+
 
