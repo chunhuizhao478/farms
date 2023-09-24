49ace280d1774568977ff8f1aace21aaeb456b21
chunhuizhao478 <chunhui3@illinois.edu>
2023-09-23 21:49:47 -0500

    sync post processing

--- postprocess/2d/netcdf4_data_extract.py
+++ postprocess/2d/netcdf4_data_extract.py
@@ -14,8 +14,10 @@ import matplotlib.pyplot as plt
 # overall_file_path = "/Volumes/One Touch/Research/RateStateDebug/2D/50m_919_fixnormal/"
 # overall_file_path = "/Volumes/One Touch/Research/RateStateDebug/2D/25m_919_fixnormal/"
 # overall_file_path = "/Volumes/One Touch/Research/RateStateDebug/2D/100m_920_dampingcomp/"
-# overall_file_path = "/Users/andyz/Downloads/100m_921_damp05/"
-overall_file_path = "/Users/andyz/Downloads/50m_921_damp05/"
+# overall_file_path = "/Volumes/One Touch/Research/RateStateDebug/2D/100m_921_damp05/"
+# overall_file_path = "/Volumes/One Touch/Research/RateStateDebug/2D/50m_921_damp05/"
+# overall_file_path = "/Volumes/One Touch/Research/RateStateDebug/2D/100m_922_uguca_setup/"
+overall_file_path = "/Volumes/One Touch/Research/RateStateDebug/2D/50m_922_uguca_setup/"
 
 #exodus_file_path = overall_file_path + "main_out.e"
 exodus_file_path = overall_file_path + "main_out.e"
@@ -30,30 +32,47 @@ exodus_file_path = overall_file_path + "main_out.e"
 # save_folder_output_file_path = "./files/100m_919_fixnormal"
 # save_folder_output_file_path = "./files/50m_919_fixnormal"
 # save_folder_output_file_path = "./files/100m_921_damp05"
-save_folder_output_file_path = "./files/50m_921_damp05"
+# save_folder_output_file_path = "./files/100m_922_uguca_setup"
+save_folder_output_file_path = "./files/50m_922_uguca_setup"
 
 decodeflag = "name_nod_var"
 
-#mid ptr locs
-# left_ptr_loc = 99
-# mid_ptr_loc = 200
-# right_ptr_loc = 302
+#mid ptr locs index
+#100m
+# ptr1_loc = 51  - 1 #-5050
+# ptr2_loc = 102 - 1 #50
+# ptr3_loc = 152 - 1 #5050
+# ptr4_loc = 76  - 1 #-2550
+# ptr5_loc = 127 - 1 #2550
+# ptr6_loc = 26  - 1 #-7550
+# ptr7_loc = 177 - 1 #7550
+
+#50m
+ptr1_loc = 101 - 1 #-5025(101) #4975(102) #5075(103)
+ptr2_loc = 202 - 1 #25
+ptr3_loc = 302 - 1 #5025
+ptr4_loc = 151 - 1 #-2525
+ptr5_loc = 252 - 1 #2525
+ptr6_loc = 51  - 1 #-7525
+ptr7_loc = 352 - 1 #7525
 
 #dt
 dt = 0.025
-t_max = 5
+t_max = 5.0
 time = np.arange(0,t_max+dt,dt)
 #save
 np.savetxt(save_folder_output_file_path + "/time.txt",time)
 
 #options
-decodenow  = True
-getvelnow = True
-getslipratenow = True
-plotnow = True
+decodenow  = False
+getvelnow = False
+getslipratenow = False
+plotnow = False
 
-getdispnow = True
-getresidnow = True
+getdispnow = False
+getresidnow = False
+peakslipratepositionfunc = False
+rupturespeed = True
 
 nc = netCDF4.Dataset(exodus_file_path)
 
@@ -111,15 +130,19 @@ if getslipratenow == True:
     #save
     np.savetxt(save_folder_output_file_path + "/sliprate.txt",sliprate)
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/sliprate_neg5kmstrike0dot75dip.txt",0.5*(sliprate[:,99]+sliprate[:,100]))
+    np.savetxt(save_folder_output_file_path + "/sliprate_neg5kmstrike0dot75dip.txt",sliprate[:,ptr1_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/sliprate_0strike0dot75dip.txt",0.5*(sliprate[:,200]+sliprate[:,201]))
+    np.savetxt(save_folder_output_file_path + "/sliprate_0strike0dot75dip.txt",sliprate[:,ptr2_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/sliprate_pos5kmstrike0dot75dip.txt",0.5*(sliprate[:,300]+sliprate[:,301]))
+    np.savetxt(save_folder_output_file_path + "/sliprate_pos5kmstrike0dot75dip.txt",sliprate[:,ptr3_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/sliprate_neg2dot5kmstrike0dot75dip.txt",0.5*(sliprate[:,149]+sliprate[:,150]))
+    np.savetxt(save_folder_output_file_path + "/sliprate_neg2dot5kmstrike0dot75dip.txt",sliprate[:,ptr4_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/sliprate_pos2dot5kmstrike0dot75dip.txt",0.5*(sliprate[:,250]+sliprate[:,251]))
+    np.savetxt(save_folder_output_file_path + "/sliprate_pos2dot5kmstrike0dot75dip.txt",sliprate[:,ptr5_loc])
+    #save middle ptrs time history
+    np.savetxt(save_folder_output_file_path + "/sliprate_neg7dot5kmstrike0dot75dip.txt",sliprate[:,ptr6_loc])
+    #save middle ptrs time history
+    np.savetxt(save_folder_output_file_path + "/sliprate_pos7dot5kmstrike0dot75dip.txt",sliprate[:,ptr7_loc])
 
 if getdispnow == True:
 
@@ -134,15 +157,19 @@ if getdispnow == True:
     #save
     np.savetxt(save_folder_output_file_path + "/slip.txt",slip)
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/slip_neg5kmstrike0dot75dip.txt",0.5*(slip[:,99]+slip[:,100]))
+    np.savetxt(save_folder_output_file_path + "/slip_neg5kmstrike0dot75dip.txt",slip[:,ptr1_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/slip_0strike0dot75dip.txt",0.5*(slip[:,200]+slip[:,201]))
+    np.savetxt(save_folder_output_file_path + "/slip_0strike0dot75dip.txt",slip[:,ptr2_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/slip_pos5kmstrike0dot75dip.txt",0.5*(slip[:,300]+slip[:,301]))
+    np.savetxt(save_folder_output_file_path + "/slip_pos5kmstrike0dot75dip.txt",slip[:,ptr3_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/slip_neg2dot5kmstrike0dot75dip.txt",0.5*(slip[:,149]+slip[:,150]))
+    np.savetxt(save_folder_output_file_path + "/slip_neg2dot5kmstrike0dot75dip.txt",slip[:,ptr4_loc])
     #save middle ptrs time history
-    np.savetxt(save_folder_output_file_path + "/slip_pos2dot5kmstrike0dot75dip.txt",0.5*(slip[:,250]+slip[:,251]))
+    np.savetxt(save_folder_output_file_path + "/slip_pos2dot5kmstrike0dot75dip.txt",slip[:,ptr5_loc])
+    #save middle ptrs time history
+    np.savetxt(save_folder_output_file_path + "/slip_neg7dot5kmstrike0dot75dip.txt",slip[:,ptr6_loc])
+    #save middle ptrs time history
+    np.savetxt(save_folder_output_file_path + "/slip_pos7dot5kmstrike0dot75dip.txt",slip[:,ptr7_loc])
 
     #get dispy
     dispy = nc.variables["vals_nod_var4"]
@@ -159,6 +186,159 @@ if getresidnow == True:
     residx_upper = residx[:,upper_ptr_index]
     #save residx
     np.savetxt(save_folder_output_file_path + "/residx_upper.txt",residx_upper)
+
+if peakslipratepositionfunc == True:
+
+    #get slip rate time series
+    sliprate = np.loadtxt(save_folder_output_file_path + "/sliprate.txt")
+
+    #load location
+    interface_coordx = np.loadtxt(save_folder_output_file_path + "/interface_coordx.txt")
+
+    sliprate_max = np.max(sliprate[:161,:],axis=0)
+
+    #get vel0
+    vel0 = np.loadtxt("/Users/andyz/projects/farms/postprocess/2d/TPV101_Nx/TPV101_Nx_50and25m/TPV101_Nx1440_s2.00_tf0.35_npc1-DataFiles/vel0.txt")
+    sliprate0 = vel0 * 2
+    sliprate_max0 = np.max(sliprate0[:40,:],axis=0)
+    xcoord0 = np.linspace(-35975,35975,720*2)
+
+    plt.plot(interface_coordx, sliprate_max, 'b.',label="moose-50m")
+    plt.plot(xcoord0, sliprate_max0, 'r.',label="uguca-50m")
+    plt.xlim([-10000,10000])
+    plt.xlabel("x coordinate (m)")
+    plt.ylabel("peak slip rate (m/s)")
+    plt.legend()
+    plt.show()
+
+if rupturespeed == True:
+
+    #load time
+    time = np.loadtxt(save_folder_output_file_path + "/time.txt")
+
+    #get slip rate time series
+    sliprate = np.loadtxt(save_folder_output_file_path + "/sliprate.txt")
+
+    #load location
+    interface_coordx = np.loadtxt(save_folder_output_file_path + "/interface_coordx.txt")
+
+    #get time
+    timeright = time[:161]
+
+    #get slip rate right half
+    sliprateright = sliprate[:161,int(np.shape(sliprate)[1]/2):]
+
+    #get location right half
+    interface_coordxright = interface_coordx[int(np.shape(sliprate)[1]/2):]
+
+    #sliprate threshold
+    threshold = 0.1
+
+    locs_plot = []
+    time_plot = []
+    index_save = []
+
+    for i in range(np.shape(sliprateright)[0]):
+
+        sliprateright_timei = sliprateright[i,:]
+        
+        index_rightmost = np.where(sliprateright_timei > threshold)[0].tolist()
+
+        if np.shape(index_rightmost)[0] >= 1:
+            index_rightmost = index_rightmost[-1]
+        else:
+            index_rightmost = 0
+
+        if i != 0:
+            if interface_coordxright[index_rightmost] < locs_plot[-1]:
+                index_rightmost = index_save[-1]
+        else:
+            index_rightmost = 0
+
+        locs_plot.append(interface_coordxright[index_rightmost])
+
+        time_plot.append(timeright[i])
+
+        index_save.append(index_rightmost)
+
+    #fit whole curve
+    polydata001_terms = np.polyfit(time_plot,locs_plot,6)
+    polydata001 = np.poly1d(polydata001_terms)
+    polyvel001_terms = np.polyder(polydata001_terms)
+    polyvel001 = np.poly1d(polyvel001_terms)
+
+    # plt.plot(time_plot,polydata001(time_plot))
+    # plt.plot(time_plot,locs_plot)
+    # plt.title("list_front_time0 vs list_front_xcoord0 or polydata001(list_front_time0)")
+    # plt.show()
+
+    # plt.plot(time_plot,polyvel001(time_plot))
+    # plt.title("list_front_time0 vs polyvel001(list_front_time0)")
+    # plt.show()
+
+    ####for uguca data
+    time = np.loadtxt("/Users/andyz/projects/farms/postprocess/2d/TPV101_Nx/TPV101_Nx_50and25m/TPV101_Nx1440_s2.00_tf0.35_npc1.time")
+    time = time[:,1]
+    #get slip rate time series
+    vel = np.loadtxt("/Users/andyz/projects/farms/postprocess/2d/TPV101_Nx/TPV101_Nx_50and25m/TPV101_Nx1440_s2.00_tf0.35_npc1-DataFiles/vel0.txt")
+    sliprate = vel * 2
+    #load location
+    interface_coordx = np.linspace(-35975,35975,720*2)
+
+    #get time
+    timeright = time[:40]
+    #get slip rate right half
+    sliprateright = sliprate[:40,int(np.shape(sliprate)[1]/2):]
+    #get location right half
+    interface_coordxright = interface_coordx[int(np.shape(sliprate)[1]/2):]
+
+    #sliprate threshold
+    threshold = 0.1
+    ugucalocs_plot = []
+    ugucatime_plot = []
+    ugucaindex_save = []
+    for i in range(np.shape(sliprateright)[0]):
+        sliprateright_timei = sliprateright[i,:]
+        index_rightmost = np.where(sliprateright_timei > threshold)[0].tolist()
+        if np.shape(index_rightmost)[0] >= 1:
+            index_rightmost = index_rightmost[-1]
+        else:
+            index_rightmost = 0
+
+        if i != 0:
+            if interface_coordxright[index_rightmost] < ugucalocs_plot[-1]:
+                index_rightmost = ugucaindex_save[-1]
+        else:
+            index_rightmost = 0
+
+        ugucalocs_plot.append(interface_coordxright[index_rightmost])
+        ugucatime_plot.append(timeright[i])
+        ugucaindex_save.append(index_rightmost)
+
+    #fit whole curve
+    ugucadata001_terms = np.polyfit(ugucatime_plot,ugucalocs_plot,6)
+    ugucadata001 = np.poly1d(ugucadata001_terms)
+    ugucavel001_terms = np.polyder(ugucadata001_terms)
+    ugucavel001 = np.poly1d(ugucavel001_terms)
+
+    # plt.plot(ugucatime_plot,ugucadata001(ugucatime_plot),'b-')
+    plt.plot(ugucatime_plot,ugucalocs_plot,'r-.',label="uguca-50m")
+    # plt.plot(time_plot,polydata001(time_plot),'r-')
+    plt.plot(time_plot,locs_plot,'b-.',label="moose-50m")
+    plt.ylim([0,10000])
+    plt.xlim([0,4])
+    plt.title("rupture tip position (m) vs time (s)")
+    plt.xlabel("time (s)")
+    plt.ylabel("rupture tip position (m)")
+    plt.legend()
+    plt.show()
+
+    plt.plot(ugucatime_plot,ugucavel001(ugucatime_plot),'b--')
+    plt.plot(time_plot,polyvel001(time_plot),'r--')
+    plt.title("list_front_time0 vs polyvel001(list_front_time0)")
+    plt.show()
+
+
 
 if plotnow == True:
 
--- postprocess/2d/sliprateprofile.m
+++ postprocess/2d/sliprateprofile.m
@@ -7,11 +7,11 @@ data1uguca = load("./TPV101_Nx/TPV101_Nx_50and25m/TPV101_Nx1440_s2.00_tf0.35_npc
 % data2uguca = load('faultEvo.txt');
 
 %moose results
-% data0 = load("./files/100m_919_fixnormal/sliprate.txt");
-% x0 = load("./files/100m_919_fixnormal/interface_coordx.txt");
+% data0 = load("./files/50m_922_uguca_setup/sliprate.txt");
+% x0 = load("./files/50m_922_uguca_setup/interface_coordx.txt");
 % 
-data2 = load("./files/50m_921_damp05/sliprate.txt");
-x2 = load("./files/50m_921_damp05/interface_coordx.txt");
+data2 = load("./files/50m_922_uguca_setup/sliprate.txt");
+x2 = load("./files/50m_922_uguca_setup/interface_coordx.txt");
 % 
 % data3 = load("./files/100m_920_dampcomp_0dot6/sliprate.txt");
 % x3 = load("./files/100m_920_dampcomp_0dot6/interface_coordx.txt");
@@ -23,19 +23,19 @@ x2 = load("./files/50m_921_damp05/interface_coordx.txt");
 % x5 = load("./files/100m_920_dampcomp_1dot0/interface_coordx.txt");
 
 filename2 = 'sliprate.gif';
-for i = 1:20
+for i = 1:40
 figure(1);
 % plot(linspace(-36000,36000,1440*2)',data0uguca(i,:)*2,'-k');hold on;
 plot(linspace(-35975,35975,720*2)',data1uguca(i,:)*2,'-k');hold on;
-% plot(linspace(-36000,36000,360*2)',data2uguca(i,:)*2,'-k');hold on;
-% plot(x0,data0(4*i-3,:),'-c'); hold on;
+% plot(linspace(-35950,35950,360*2)',data2uguca(i,:)*2,'-k');hold on;
+% plot(x0,data0(4*i-3,:),'-b'); hold on;
 plot(x2,data2(4*i-3,:),'-b'); hold on;
 % plot(x3,data3(4*i-3,:),'-k'); hold on;
 % plot(x4,data4(4*i-3,:),'-m'); hold on;
 % plot(x5,data5(4*i-3,:),'-b'); hold on;
 hold off;
-xlim([-2.5e3 2.5e3])
-% legend('uguca-25m','uguca-50m','uguca-100m','moose-100m',FontSize=10)
+xlim([-15e3 15e3])
+legend('uguca-50m','moose-50m','location','northeast',FontSize=10)
 % legend('uguca-100m','MOOSE-100m-damp-1.0','Location','northoutside',FontSize=10)
 % legend('uguca-100m','MOOSE-100m-damp-0.2','MOOSE-100m-damp-0.4','MOOSE-100m-damp-0.6','MOOSE-100m-damp-0.8','MOOSE-100m-damp-1.0',FontSize=10)
 title("SCEC Benchmark TPV101","FontSize",30)
