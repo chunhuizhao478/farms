2dc92e3e4097f8788083b3ebe645d939d5e81c6b
chunhuizhao478 <chunhui3@illinois.edu>
2023-10-03 17:58:03 -0500

    sync lab test

--- examples/cdbm_labtest/Cdtest/plotfigures.m
+++ examples/cdbm_labtest/Cdtest/plotfigures.m
@@ -27,10 +27,23 @@ ylabel("damage variable")
 
 xiCd40e1_eta0dot8 = readmatrix("xiCd40e1eta0dot8.txt");
 alphaCd40e1_eta0dot8 = readmatrix("alphaCd40e1eta0dot8.txt");
-xiCd40e1_eta0dot8 = readmatrix("xiCd40e1eta0dot8.txt");
-alphaCd40e1_eta0dot8 = readmatrix("alphaCd40e1eta0dot8.txt");
+xiCd40e1_eta0dot1 = readmatrix("xiCd40e1eta0dot1.txt");
+alphaCd40e1_eta0dot1 = readmatrix("alphaCd40e1eta0dot1.txt");
 
 figure();
 plot(xiCd40e1_eta0dot8,alphaCd40e1_eta0dot8,'r-'); hold on;
 plot(xiCd40e1,alphaCd40e1,'k-'); hold on;
 plot(xiCd40e1_eta0dot1,alphaCd40e1_eta0dot1,'b-'); hold on;
+
+legend("Cd=40e1,Cb=100Cd,eta=0.8","Cd=40e4,Cb=100Cd,eta=0.5","Cd=40e3,Cb=100Cd,eta=0.1")
+
+title("damage variable vs strain invariant ratio")
+xlabel("strain invariant ratio")
+ylabel("damage variable")
+
+xiCd40e1_xi01dot1 = readmatrix("xiCd40e1xi01dot0.txt");
+alphaCd40e1_xi01dot1 = readmatrix("alphaCd40e1xi01dot0.txt");
+
+figure();
+plot(xiCd40e1_xi01dot1,alphaCd40e1_xi01dot1,'r-'); hold on;
+plot(xiCd40e1,alphaCd40e1,'k-'); hold on;
