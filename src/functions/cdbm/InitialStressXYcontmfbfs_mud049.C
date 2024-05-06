4bf1be4596ee504a810dd915f4cb85f9b67e4f64
chunhuizhao478 <chunhui3@illinois.edu>
2024-05-05 19:44:15 -0500

    change nucleation length to cover frictional length

--- src/functions/cdbm/InitialStressXYcontmfbfs_mud049.C
+++ src/functions/cdbm/InitialStressXYcontmfbfs_mud049.C
@@ -23,7 +23,7 @@ InitialStressXYcontmfbfs_mud049::value(Real /*t*/, const Point & p) const
 
   Real T1_o = 0;
 
-  if (x_coord >= -286 and x_coord <= 286 and y_coord >= -100 and y_coord <= 100)
+  if (x_coord >= -400 and x_coord <= 400 and y_coord >= -100 and y_coord <= 100)
   {
     //T1_o = 81.6e6;
     //raise overstress to 1%
