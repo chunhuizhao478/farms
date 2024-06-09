0d8e111099a1809a6febbf17a55ac021317244bc
chunhuizhao478 <chunhui3@illinois.edu>
2024-06-09 15:29:28 -0600

    fix requriedparam issue base class

--- src/materials/slipweakening/CZMComputeLocalTractionBaseLSW3D.C
+++ src/materials/slipweakening/CZMComputeLocalTractionBaseLSW3D.C
@@ -10,11 +10,6 @@ CZMComputeLocalTractionBaseLSW3D::validParams()
                              "models that can be formulated using the total displacement jump");
   params.addRequiredCoupledVar("displacements",
                                "The string of displacements suitable for the problem statement");
-  params.addRequiredParam<Real>("Tn_o","initial normal traction");
-  params.addRequiredParam<Real>("Ts_o","initial strike traction");
-  params.addRequiredParam<Real>("Td_o","initial dip traction");
-  params.addRequiredParam<Real>("Vini","initial velocity");
-  params.addRequiredParam<Real>("statevarini","initial state variable");
   params.suppressParameter<bool>("use_displaced_mesh");
   params.addParam<std::string>("base_name", "Material property base name");
   return params;
