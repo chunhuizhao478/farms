3f1545ed4812071e3ddee67c7cb2966cb5c5b77c
chunhuizhao478 <chunhui3@illinois.edu>
2024-06-17 22:52:41 -0600

    fix area of triangle slip weakening

--- src/materials/slipweakening/SlipWeakeningMultifaults3D.C
+++ src/materials/slipweakening/SlipWeakeningMultifaults3D.C
@@ -175,10 +175,13 @@ SlipWeakeningMultifaults3D::computeInterfaceTractionAndDerivatives()
     //Compute node mass //equal length tetrahedron
     Real M = _density[_qp] * sqrt(3) / 8 * area * area * area / 3;
 
+    //Compute area of a triangle (assume equal lengths)
+    Real area_of_triangle = sqrt(3) / 4 * area * area;
+
     //Compute sticking stress
-    Real T1 =   (1/_dt)*M*displacement_jump_rate(1)/(2*area*area) + (R_plus_local_x - R_minus_local_x)/(2*area*area) + T1_o;
-    Real T3 =   (1/_dt)*M*displacement_jump_rate(2)/(2*area*area) + (R_plus_local_z - R_minus_local_z)/(2*area*area) + T3_o;
-    Real T2 =  -(1/_dt)*M*(displacement_jump_rate(0)+(1/_dt)*displacement_jump(0))/(2*area*area) + ( (R_minus_local_y - R_plus_local_y) / ( 2*area*area ) ) - T2_o ;
+    Real T1 =   (1/_dt)*M*displacement_jump_rate(1)/(2*area_of_triangle) + (R_plus_local_x - R_minus_local_x)/(2*area_of_triangle) + T1_o;
+    Real T3 =   (1/_dt)*M*displacement_jump_rate(2)/(2*area_of_triangle) + (R_plus_local_z - R_minus_local_z)/(2*area_of_triangle) + T3_o;
+    Real T2 =  -(1/_dt)*M*(displacement_jump_rate(0)+(1/_dt)*displacement_jump(0))/(2*area_of_triangle) + ( (R_minus_local_y - R_plus_local_y) / ( 2*area_of_triangle ) ) - T2_o ;
 
     //Note: The distance that the node has slipped is path-integrated. For example, if the node slips 0.4 m in one
     //direction and then 0.1 m in the opposite direction, the value of is 0.5 m (and not 0.3 m).
