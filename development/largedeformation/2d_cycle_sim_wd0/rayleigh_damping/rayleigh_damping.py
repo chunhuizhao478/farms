74326e0883da7b809e5bcefdebc613f48e14c28e
chunhuizhao478 <chunhui3@illinois.edu>
2024-11-08 22:58:30 -0600

    cdbm ld: (1)  set case 6: quadratic, w absorber, don't apply on top and bottom (2) set case 7: quadratic, w absorber, don't apply on top and bottom, add Rayleigh damping

---
+++ development/largedeformation/2d_cycle_sim_wd0/rayleigh_damping/rayleigh_damping.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Given data
+zeta = 0.01 # desired damping ratio
+fmin = 19  # Hz
+fmax = 100  # Hz
+
+omega_min = 2 * np.pi * fmin  # rad/s
+omega_max = 2 * np.pi * fmax  # rad/s
+
+# Calculation
+A = np.array([[1/omega_min, omega_min],
+              [1/omega_max, omega_max]])
+b = np.array([2*zeta, 2*zeta])
+alpha, beta = np.linalg.solve(A, b)
+
+print("alpha : ", alpha, "beta : ", beta)
