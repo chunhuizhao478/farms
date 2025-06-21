030f3d0c46d839f6a008d3e8620fbfba1bc9f13d
chunhuizhao478 <chunhui3@illinois.edu>
2025-06-21 11:04:33 -0500

    cdbm benchmark: test

---
+++ examples/benchmark_cdbm/README.md
@@ -0,0 +1,2 @@
+Continuum Damage Breakage Rheology Benchmark
+Created By Chunhui Zhao, Jun 21, 2025 
---
+++ include/functions/slipweakeningczm/InitialShearStressCDBM.h
@@ -0,0 +1,40 @@
+/*
+Define Function for Initial Static Friction Coefficient for benchmark
+*/
+
+#pragma once
+
+#include "Function.h"
+
+class SolutionUserObjectBase;
+
+class InitialShearStressCDBM : public Function
+{
+public:
+  InitialShearStressCDBM(const InputParameters & parameters);
+
+  static InputParameters validParams();
+
+  using Function::value;
+  virtual Real value(Real t, const Point & p) const override;
+
+  /**
+   * Setup the function for use
+   * Gathers a pointer to the SolutionUserObject containing the solution that
+   * was read. A pointer is required because Functions are created prior to UserObjects,
+   * see Moose.C.
+   */
+  virtual void initialSetup() override;
+
+  Real _peak_value;
+
+  Real _nucl_center_x;
+  Real _nucl_center_z;
+
+  Real _nucl_size;
+  
+  Real _elem_size;
+
+  Real _domain_value;
+
+};
