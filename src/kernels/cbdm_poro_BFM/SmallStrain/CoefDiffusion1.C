023c8dc54eda6827959d0c706ee342208d483224
AmrIbrahim8894 <amri2@illinois.edu>
2025-12-02 21:00:41 -0600

    3D dynamic rupture problem

---
+++ src/kernels/cbdm_poro_BFM/SmallStrain/CoefDiffusion1.C
@@ -0,0 +1,54 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "CoefDiffusion1.h"
+
+registerMooseObject("farmsApp", CoefDiffusion1);
+
+InputParameters
+CoefDiffusion1::validParams()
+{
+  InputParameters params = Kernel::validParams();
+  params.addParam<Real>("coef", 0.0, "Diffusion coefficient");
+  params.addParam<FunctionName>("function",
+                                "If provided, the diffusion coefficient will be coef + "
+                                "this function.  This is useful for temporally or "
+                                "spatially varying diffusivities");
+  params.addClassDescription("Kernel for diffusion with diffusivity = coef + function");
+  return params;
+}
+
+CoefDiffusion1::CoefDiffusion1(const InputParameters & parameters)
+  : Kernel(parameters),
+    _coef(getParam<Real>("coef")),
+    _func(parameters.isParamValid("function") ? &getFunction("function") : NULL)
+{
+}
+
+Real
+CoefDiffusion1::computeQpResidual()
+{
+  Real diffusivity = _coef;
+
+  if (_func)
+    diffusivity += _func->value(_t, _q_point[_qp]);
+
+  return diffusivity * _grad_test[_i][_qp] * _grad_u[_qp];
+}
+
+Real
+CoefDiffusion1::computeQpJacobian()
+{
+  Real diffusivity = _coef;
+
+  if (_func)
+    diffusivity += _func->value(_t, _q_point[_qp]);
+
+  return diffusivity * _grad_test[_i][_qp] * _grad_phi[_j][_qp];
+}
