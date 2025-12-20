299d517c16f5b63740c08c1c3b13b11c8208c400
AmrIbrahim8894 <amri2@illinois.edu>
2025-12-20 00:29:36 -0600

    3D dynamic rupture problem

--- src/materials/slipweakeningczm3d/PoroSlipWeakeningFrictionczm3dCDBM.C
+++ src/materials/slipweakeningczm3d/PoroSlipWeakeningFrictionczm3dCDBM.C
@@ -552,16 +552,21 @@ PoroSlipWeakeningFrictionczm3dCDBM::computeInterfaceTractionAndDerivatives()
 //   }
 
   // Compute node mass and area
+  // Real M = 0;
+  // Real A = 0;
+  // if (_current_elem->type() == libMesh::ElemType::TET4){
+  //   M = (_density[_qp] * sqrt(2) * _len * _len * _len / 12 / 4) * 6;
+  //   A = (sqrt(3) * _len * _len / 4 / 3) * 6;
+  // }
+  // else if (_current_elem->type() == libMesh::ElemType::HEX8){
+  //   M = (_density[_qp] * _len * _len * _len / 8) * 4;
+  //   A = (_len * _len / 4) * 4;
+  // }
+
   Real M = 0;
   Real A = 0;
-  if (_current_elem->type() == libMesh::ElemType::TET4){
-    M = (_density[_qp] * sqrt(2) * _len * _len * _len / 12 / 4) * 6;
-    A = (sqrt(3) * _len * _len / 4 / 3) * 6;
-  }
-  else if (_current_elem->type() == libMesh::ElemType::HEX8){
-    M = (_density[_qp] * _len * _len * _len / 8) * 4;
-    A = (_len * _len / 4) * 4;
-  }
+    M = (_density[_qp] * _len * _len * _len) / 2;
+    A = _len * _len;
 
   // Compute T1_o, T2_o, T3_o for current qp
   //!!! rotation matrix is not applied here !!!
